7fcf50b9342a7444c7c0143f10970c4c5d7c2c68
Vasilij Belosludcev <bupy765@gmail.com>
2016-02-24 01:20:26 +0500

    rollback PhpDebugBarFactory

--- src/ZfSnapPhpDebugBar/Service/PhpDebugBarFactory.php
+++ src/ZfSnapPhpDebugBar/Service/PhpDebugBarFactory.php
@@ -38,7 +38,7 @@ class PhpDebugBarFactory implements FactoryInterface
         $debugbar->addCollector(new ConfigCollector($appConfig, 'ApplicationConfig'));
 
         // Db profiler
-        if ($serviceLocator->has($dbServiceName)) {
+        if ($serviceLocator->has($dbServiceName) && isset($config['db']['driver'])) {
             $adapter = $serviceLocator->get($dbServiceName);
             $this->prepareDbAdapter($adapter, $debugbar);
         }
