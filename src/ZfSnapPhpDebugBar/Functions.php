c78040f6d3a98a78a0de5ee93fdd48e36b7f7b07
Layton Everson <layton.everson@gmail.com>
2016-04-11 14:50:50 -0600

    Check function_exists before declairing debugbar_log() function.

--- src/ZfSnapPhpDebugBar/Functions.php
+++ src/ZfSnapPhpDebugBar/Functions.php
@@ -1,10 +1,12 @@
 <?php
 
-/**
- * @param string $message
- * @param string $type
- */
-function debugbar_log($message, $type = 'debug')
-{
-    \ZfSnapPhpDebugBar\Module::log($message, $type);
+if (!function_exists('debugbar_log')) {
+    /**
+     * @param string $message
+     * @param string $type
+     */
+    function debugbar_log($message, $type = 'debug')
+    {
+        \ZfSnapPhpDebugBar\Module::log($message, $type);
+    }
 }
