1e103b10d29d68d4d41973293dd8013539919dca
Witold Wasiczko <witold@wasiczko.pl>
2016-10-23 19:05:09 +0200

    Add aliases config

---
+++ config/aliases.config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Aliases for ZF3 (it's case sensitive)
+ */
+return [
+    'service_manager' => [
+        'aliases' => [
+            'DebugBar' => 'debugbar',
+            'Debugbar' => 'debugbar',
+        ],
+    ],
+    'view_helpers' => [
+        'aliases' => [
+            'DebugBar' => 'debugbar',
+            'Debugbar' => 'debugbar',
+        ],
+    ],
+];
