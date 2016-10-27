a59438ce177e96a607af441f0c77a59543596d32
Vasilij Belosludcev <bupy765@gmail.com>
2016-10-27 11:44:55 +0500

    added integration Doctrine Queries #6

---
+++ src/Collector/DoctrineCollectorFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ZfSnapPhpDebugBar\Collector;
+
+use Interop\Container\ContainerInterface;
+use DebugBar\Bridge\DoctrineCollector;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class DoctrineCollectorFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $debugStack = $container->get('doctrine.configuration.orm_default')->getSQLLogger();
+        return new DoctrineCollector($debugStack);
+    }
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return $this($serviceLocator, DoctrineCollector::class);
+    }
+}
